647d7f5c8139724eafb80d7c5ad3d5c5a501b676
BashPrime <5769306+BashPrime@users.noreply.github.com>
2024-12-02 18:32:15 -0800

    add blurb

---
+++ worlds/metroidprime2/__init__.py
@@ -0,0 +1,11 @@
+from worlds.AutoWorld import World
+
+class MetroidPrime2World(World):
+    """
+    Metroid Prime 2: Echoes is a first-person action-adventure game. You once again play as
+    bounty hunter Samus Aran, who is sent to the rogue planet Aether to investigate the
+    disappearance of a Galactic Federation Marine Corps task force. She ends up in the
+    middle of a decades-long war between the Luminoth and the Ing over the dimensionally-torn
+    planet, and must survive the hostile conditions of both Aether and Dark Aether to accomplish
+    her mission.
+    """
